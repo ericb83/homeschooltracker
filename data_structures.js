87894db5ee68d59955bbc3f3a90b9fa9ad098ae5
ericb83 <eboerschinger@gmail.com>
2024-05-22 07:45:05 -0500

    Initial commit

---
+++ data_structures.js
@@ -0,0 +1,17 @@
+const user_data={
+    name: "John Doe",
+    email:"email@example.com",
+    password:"password",
+    phone:"(123)456-7890",
+    
+}
+const stored_data={
+    school_name:"Abc School",
+    student:"John Doe",
+    course:"Math",
+    hours:"1",
+    total_hours:"800",
+    avg_hours:"4",
+    grade:"A",
+    GPA:"4.0",
+}
