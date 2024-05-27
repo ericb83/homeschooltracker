71ceace55b34e80fced2545007e79b3ca71c8de6
ericb83 <eboerschinger@gmail.com>
2024-05-26 22:08:39 -0500

    add image and flexbox styling

--- data_structures.js
+++ data_structures.js
@@ -1,11 +1,13 @@
 const user_data={
-    UID:"273hdh34889338"
+    UID:"273hdh348893",
     name: "John Doe",
     email:"email@example.com",
     phone:"(123)456-7890",
     
 }
 const stored_data={
+    parent_id:"hdfu37fgfu47",
+    student_id:"guue9725gt56",
     school_name:"Abc School",
     school_address:"123 Any Rd, Anytown,USA",
     student_name:"John Doe",
